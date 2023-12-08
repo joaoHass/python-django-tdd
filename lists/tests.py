a176542b4f7035f66aea9fa4056029b3b424a039
João Hass <joaopedrohass@outlook.com>
2023-12-08 18:26:15 -0300

    refact home_page tests into one test
    they tested the same thing, but were made separately for learning
    purposes.

--- lists/tests.py
+++ lists/tests.py
@@ -2,18 +2,12 @@ from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
 
-def test_home_page_returns_correct_html():
-    request = HttpRequest()
-    
-    response = home_page(request)
-    html = response.content.decode('utf8')
-    
-    assert '<title>To-Do Lists</title>' in html
-    assert html.startswith('<html>')
-    assert html.endswith('</html>')
-    
 # utilizes the django.test.Client fixture from pytest-django
 # see: https://pytest-django.readthedocs.io/en/latest/helpers.html#client-django-test-client
-def test_home_page_returns_correct_html2(client):
-    assert '<title>To-Do Lists</title>' in client.get('/').content.decode('utf8')
+def test_home_page_returns_correct_html(client):
+    response_html = client.get('/').content.decode('utf8')
+    
+    assert '<title>To-Do Lists</title>' in response_html
+    assert response_html.startswith('<html>')
+    assert response_html.endswith('</html>')
     
