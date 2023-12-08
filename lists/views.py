1c1174bc4d513a384b7831c77f3ea1d922caee8d
João Hass <joaopedrohass@outlook.com>
2023-12-08 14:50:43 -0300

    test(green) home_pages now returns the correct html

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from typing import Type
+from django.http import HttpRequest
+from django.http import HttpResponse
+
 
 # Create your views here.
+def home_page(request: Type[HttpRequest]):
+    return HttpResponse('<html><title>To-Do Lists</title></html>')
