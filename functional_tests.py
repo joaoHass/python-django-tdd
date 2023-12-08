279af77090bfa26bf5070542208e7330c22e7b04
João Hass <joaopedrohass@outlook.com>
2023-12-08 18:58:35 -0300

    feat functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,8 @@
-import pytest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import pytest
+import time
 
 @pytest.fixture
 def setup_data():
@@ -14,6 +17,35 @@ def setup_data():
 def test_can_start_a_todo_list(setup_data):
     browser = setup_data
     
+    # Edith has heard about a cool new online to-do app.
+    # She goes to check out its homepage
     browser.get('http://localhost:8000')
     
+    # She notices the page title and header mention to-do lists
     assert 'To-Do' in browser.title
+    h1_text = browser.find_element(By.TAG_NAME, 'h1').text
+    assert 'To-Do' in h1_text
+    
+    # She is invited to enter a to-do item straight away
+    inputbox = browser.find_element(BY.ID, 'idNewItem')
+    assert inputbox.get_attribute('placeholder', 'Enter a To-Do item')
+    
+    # She types "Buy peacock feathers" into a text box
+    # (Edith's hobby is tying fly-fishing lures)
+    inputbox.send_keys('Buy peacock feathers')
+    
+    # When she hits enter, the page updates, and now the page lists
+    # "1: Buy peacock feathers" as an item in a to-do list table\
+    inputbox.send_keys(Keys.ENTER)
+    time.sleep(1)
+    
+    table = browser.find.element(By.ID, 'idListTable')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+    assert any(row.text == '1: Buy peacock feathers' for row in rows)
+    
+    # There is still a text box inviting her to add another item.
+    # She enters "Use peacock feathers to make a fly"
+    # (Edith is very methodical)
+    pytest.fail('Finish the test')
+    
+    # The page updates again, and now shows both items on her list
