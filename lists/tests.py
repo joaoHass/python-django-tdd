d150e31cd9e38954c86238e776b2b82c07e53baa
João Hass <joaopedrohass@outlook.com>
2023-12-08 18:20:46 -0300

    test(red) checks home_page title using url route
    Originally it would be written using assertContains from
    the django test suit, but I couldn't put the pycharm equivalent
    to work. Because of that, I went with the .content.decode() route,
    which work fairly well.
    
    the reason I couldn't make the pycharm equivalent work (the
    equivalent being ' from pytest_django.asserts import assertContains')
    is because when I try to assertContains to check for the html of
    the request, it checks inside the:
      '<HttpResponse status_code=200, "text/html; charset=utf-8">'
    when it should be checking for the content of the response.

--- lists/tests.py
+++ lists/tests.py
@@ -12,3 +12,8 @@ def test_home_page_returns_correct_html():
     assert html.startswith('<html>')
     assert html.endswith('</html>')
     
+# utilizes the django.test.Client fixture from pytest-django
+# see: https://pytest-django.readthedocs.io/en/latest/helpers.html#client-django-test-client
+def test_home_page_returns_correct_html2(client):
+    assert '<title>To-Do Lists</title>' in client.get('/').content.decode('utf8')
+    
