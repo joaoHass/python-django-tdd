9df08955a59a542774c2fe272c9f64400fafaa2a
João Hass <joaopedrohass@outlook.com>
2023-12-08 14:15:57 -0300

    test(red) test if home_page returns the correct html

--- lists/tests.py
+++ lists/tests.py
@@ -1,1 +1,14 @@
-# Create your tests here.
+from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
+
+def test_home_page_returns_correct_html():
+    request = HttpRequest()
+    
+    response = home_page(request)
+    html = response.content.decode('utf8')
+    
+    assert '<title>To-Do Lists</title>' in html
+    assert html.startsWith('<html>')
+    assert html.endsWith('<html>')
+    
