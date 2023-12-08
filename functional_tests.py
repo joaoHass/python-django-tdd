2500fc36d3f2d0f8c597a371e78b22b566648dcb
João Hass <joaopedrohass@outlook.com>
2023-12-08 13:33:03 -0300

    refact test now uses unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,19 @@
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox();
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_todo_list(self):
+        self.browser.get('http://localhost:8000')
+        
+        self.assertIn('To-Do', self.browser.title)
+        
+        self.fail('Finish the test!')
 
-assert 'Congratulations!' in browser.title
-print('OK')
+if __name__ == "__main__":
+    unittest.main()
