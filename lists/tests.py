0d6d8e55d8d984792353bbed21b27fce1c214b0d
João Hass <joaopedrohass@outlook.com>
2023-12-08 22:06:34 -0300

    test(green) fix home_page test for correct html
    as the html for the home_page was refactored from
    being generated inside the function to a dedicated template,
    I had to take in consideration two new things:
    
    1) the DOCTYPE tag at the start of the html (generated by emmet)
    2) whitespaces at the end of the file (sure, we can get rid of them
    directly in the template, but they have no meaning and don't change
    anything, so if we have leading whitespaces or not does not matter
    from a functional perspective)
    
    to accomodate these two new things, I changed the test to take these
    into consderation.

--- lists/tests.py
+++ lists/tests.py
@@ -1,13 +1,15 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from pytest_django.asserts import assertTemplateUsed
+
 
 # utilizes the django.test.Client fixture from pytest-django
 # see: https://pytest-django.readthedocs.io/en/latest/helpers.html#client-django-test-client
 def test_home_page_returns_correct_html(client):
-    response_html = client.get('/').content.decode('utf8')
+    response_html = client.get('/').content.decode('utf8').strip()
     
     assert '<title>To-Do Lists</title>' in response_html
-    assert response_html.startswith('<html>')
+    assert response_html.startswith('<html>') or response_html.startswith('<!DOCTYPE html>')
     assert response_html.endswith('</html>')
     
