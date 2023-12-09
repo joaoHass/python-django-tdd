bedf2362828aa7fc694489a5b18b142edf2be4c6
João Hass <joaopedrohass@outlook.com>
2023-12-08 22:02:21 -0300

    feat add template to home_page view

--- lists/views.py
+++ lists/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 from typing import Type
 from django.http import HttpRequest
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 # Create your views here.
 def home_page(request: Type[HttpRequest]):
-    return HttpResponse('<html><title>To-Do Lists</title></html>')
+    return render(request, 'home.html')
