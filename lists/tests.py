2c8e557cd6aca177a38a7f221bb1c42d317525fb
João Hass <joaopedrohass@outlook.com>
2023-12-08 22:15:22 -0300

    chore add assertion for template in home_page test

--- lists/tests.py
+++ lists/tests.py
@@ -7,9 +7,11 @@ from pytest_django.asserts import assertTemplateUsed
 # utilizes the django.test.Client fixture from pytest-django
 # see: https://pytest-django.readthedocs.io/en/latest/helpers.html#client-django-test-client
 def test_home_page_returns_correct_html(client):
-    response_html = client.get('/').content.decode('utf8').strip()
+    response = client.get('/')
+    response_html = response.content.decode('utf8').strip()
     
     assert '<title>To-Do Lists</title>' in response_html
     assert response_html.startswith('<html>') or response_html.startswith('<!DOCTYPE html>')
     assert response_html.endswith('</html>')
+    assertTemplateUsed(response, 'home.html')
     
