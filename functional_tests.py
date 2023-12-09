980d4b509c16751690987d61ee7861789d30315e
João Hass <joaopedrohass@outlook.com>
2023-12-09 01:20:46 -0300

    refact create function for row checking in table

--- functional_tests.py
+++ functional_tests.py
@@ -41,9 +41,8 @@ def test_can_start_a_todo_list(setup_data):
     
     table = browser.find_element(By.ID, 'idListTable')
     rows = table.find_elements(By.TAG_NAME, 'tr')
-    assert '1: Buy peacock feathers' in [row.text for row in rows]
-    assert '2: Use peacock feathers to make a fly' in [row.text for row in rows]
-    
+    check_for_row_in_table_list(browser, '1: Buy peacock feathers')
+    check_for_row_in_table_list(browser, '2: Use peacock feathers to make a fly')
     
     # There is still a text box inviting her to add another item.
     # She enters "Use peacock feathers to make a fly"
@@ -51,3 +50,9 @@ def test_can_start_a_todo_list(setup_data):
     pytest.fail('Finish the test')
     
     # The page updates again, and now shows both items on her list
+
+def check_for_row_in_table_list(browser, row_text):
+    table = browser.find_element(By.ID, 'idListTable')
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+    
+    assert row_text in [row.text for row in rows]
